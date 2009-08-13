f1ef0a338807512580cac2583ff720b3e0f2337d
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2009-08-13 16:22:44 +0000

    install scripts for service script

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@
 install -d -m 755 $DESTDIR/usr/sbin
 install -d -m 755 $DESTDIR/usr/share/applications
 install -d -m 755 $DESTDIR/usr/share/salixtools/pixmaps
+install -d -m 755 $DESTDIR/usr/share/salixtools/servicesetup
+install -d -m 755 $DESTDIR/etc/rc.d/desc.d
 
-for i in clocksetup keyboardsetup localesetup usersetup; do
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup; do
 	install -m 755 $i/$i $DESTDIR/usr/sbin/
 	install -m 644 $i/$i.desktop $DESTDIR/usr/share/applications/
 	install -m 644 $i/$i-kde.desktop $DESTDIR/usr/share/applications/
@@ -16,6 +18,14 @@ for i in clocksetup keyboardsetup localesetup usersetup; do
 		install -m 644 $j \
 		$DESTDIR/usr/share/locale/`basename $j|sed "s/$i-//"|sed "s/.mo//"`/LC_MESSAGES/$i.mo
 	done
+done
+install -m 755 servicesetup/service $DESTDIR/usr/sbin/
+for i in `ls servicesetup/locale/service-*.mo`; do
+	install -d -m 755 \
+	$DESTDIR/usr/share/locale/`basename $i|sed "s/service-\(.*\).mo/\1/"`/LC_MESSAGES \
+	2> /dev/null
+	install -m 644 $i \
+	$DESTDIR/usr/share/locale/`basename $i|sed "s/service-\(.*\).mo/\1"`/LC_MESSAGES/service.mo
 done
 
 for i in `ls icons/*.svg`; do
@@ -38,3 +48,7 @@ for i in `ls pixmaps/*`; do
 done
 
 install -m 644 keyboardsetup/keymaps $DESTDIR/usr/share/salixtools/
+
+install -m 644 servicesetup/service-blacklist $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 servicesetup/shell-colours $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 servicesetup/standard.txt $DESTDIR/etc/rc.d/desc.d/
