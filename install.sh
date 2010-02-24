078584c0399d423aeace776bc62526cb5718618c
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-02-24 11:24:06 +0000

    Removed icons and .desktop files from installation script

--- install.sh
+++ install.sh
@@ -3,15 +3,11 @@
 
 cd $(dirname $0)
 install -d -m 755 $DESTDIR/usr/sbin
-install -d -m 755 $DESTDIR/usr/share/applications
-install -d -m 755 $DESTDIR/usr/share/salixtools/pixmaps
 install -d -m 755 $DESTDIR/usr/share/salixtools/servicesetup
 install -d -m 755 $DESTDIR/etc/rc.d/desc.d
 
 for i in clocksetup keyboardsetup localesetup usersetup servicesetup; do
 	install -m 755 $i/$i $DESTDIR/usr/sbin/
-	install -m 644 $i/$i.desktop $DESTDIR/usr/share/applications/
-	install -m 644 $i/$i-kde.desktop $DESTDIR/usr/share/applications/
 	for j in `ls $i/locale/$i-*.mo`; do
 		install -d -m 755 \
 		$DESTDIR/usr/share/locale/`basename $j|sed "s/$i-//"|sed "s/.mo//"`/LC_MESSAGES \
@@ -37,25 +33,6 @@ for i in clocksetup keyboardsetup localesetup usersetup servicesetup; do
 		install -m644 $j $DESTDIR/usr/man/man8/
 	done
 	)
-done
-
-for i in `ls icons/*.svg`; do
-	install -d -m 755 $DESTDIR/usr/share/icons/hicolor/scalable/apps/
-	install -m 644 $i $DESTDIR/usr/share/icons/hicolor/scalable/apps/
-done
-
-for i in 64 48 32 24 22 16; do
-	for j in `ls icons/*-$i.png 2> /dev/null`; do
-		install -d -m 755 \
-		$DESTDIR/usr/share/icons/hicolor/${i}x${i}/apps/ \
-		2> /dev/null
-		install -m 644 $j \
-		$DESTDIR/usr/share/icons/hicolor/${i}x${i}/apps/`basename $j|sed "s/-$i//"`
-	done
-done
-
-for i in `ls pixmaps/*`; do
-	install -m 644 $i $DESTDIR/usr/share/salixtools/pixmaps/
 done
 
 install -m 644 keyboardsetup/keymaps $DESTDIR/usr/share/salixtools/
