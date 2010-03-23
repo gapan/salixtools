1599fd4e7ed546831fd71aa86f43ad36c56ef2d7
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-03-23 00:07:00 +0000

    Split service from servicesetup Pt3

--- compile.sh
+++ compile.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 cd $(dirname $0)
-for i in clocksetup keyboardsetup localesetup usersetup servicesetup;do
-	for j in `ls $i/locale/$i-*.po`;do
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service;do
+	for j in `ls $i/locale/*.po`;do
 		echo "Compiling `echo $j|sed "s|/locale||"`"
 		msgfmt $j -o `echo $j | sed "s/.po//"`.mo
 	done
 done
-for i in `ls servicesetup/locale/service-*.po`;do
-	echo "Compiling `echo $i|sed "s|/locale||"`"
-	msgfmt $i -o `echo $i | sed "s/.po//"`.mo
-done
-for i in clocksetup keyboardsetup localesetup usersetup servicesetup;do
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service;do
 	(
 	cd $i/man
 	echo "Compiling $i manpage"
--- install.sh
+++ install.sh
@@ -6,27 +6,19 @@ install -d -m 755 $DESTDIR/usr/sbin
 install -d -m 755 $DESTDIR/usr/share/salixtools/servicesetup
 install -d -m 755 $DESTDIR/etc/rc.d/desc.d
 
-for i in clocksetup keyboardsetup localesetup usersetup servicesetup; do
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service; do
 	install -m 755 $i/$i $DESTDIR/usr/sbin/
-	for j in `ls $i/locale/$i-*.mo`; do
+	for j in `ls $i/locale/*.mo`; do
 		install -d -m 755 \
-		$DESTDIR/usr/share/locale/`basename $j|sed "s/$i-//"|sed "s/.mo//"`/LC_MESSAGES \
+		$DESTDIR/usr/share/locale/`basename $j|sed "s/.mo//"`/LC_MESSAGES \
 		2> /dev/null
 		install -m 644 $j \
-		$DESTDIR/usr/share/locale/`basename $j|sed "s/$i-//"|sed "s/.mo//"`/LC_MESSAGES/$i.mo
+		$DESTDIR/usr/share/locale/`basename $j|sed "s/.mo//"`/LC_MESSAGES/$i.mo
 	done
-done
-install -m 755 servicesetup/service $DESTDIR/usr/sbin/
-for i in `ls servicesetup/locale/service-*.mo`; do
-	install -d -m 755 \
-	$DESTDIR/usr/share/locale/`basename $i|sed "s/service-\(.*\).mo/\1/"`/LC_MESSAGES \
-	2> /dev/null
-	install -m 644 $i \
-	$DESTDIR/usr/share/locale/`basename $i|sed "s/service-\(.*\).mo/\1/"`/LC_MESSAGES/service.mo
 done
 
 install -d -m 755 $DESTDIR/usr/man/man8
-for i in clocksetup keyboardsetup localesetup usersetup servicesetup; do
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service; do
 	(
 	cd $i/man
 	for j in `ls *.8`; do
@@ -38,7 +30,7 @@ done
 install -m 644 keyboardsetup/keymaps $DESTDIR/usr/share/salixtools/
 install -m 755 keyboardsetup/rc.numlock $DESTDIR/etc/rc.d/
 
-install -m 644 servicesetup/service-blacklist $DESTDIR/usr/share/salixtools/servicesetup/
-install -m 644 servicesetup/shell-colours $DESTDIR/usr/share/salixtools/servicesetup/
-install -m 644 servicesetup/standard.txt $DESTDIR/etc/rc.d/desc.d/
+install -m 644 service/service-blacklist $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 service/shell-colours $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 service/standard.txt $DESTDIR/etc/rc.d/desc.d/
 
