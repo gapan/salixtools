646368bbe0bab9382d0cfad9f6048c79391053e1
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-03-26 10:21:47 +0000

    Tagged for 1.0.0 release

---
+++ trunk/compile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd $(dirname $0)
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service;do
+	for j in `ls $i/locale/*.po`;do
+		echo "Compiling `echo $j|sed "s|/locale||"`"
+		msgfmt $j -o `echo $j | sed "s/.po//"`.mo
+	done
+done
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service;do
+	(
+	cd $i/man
+	echo "Compiling $i manpage"
+	txt2tags *.t2t || exit 1
+	#txt2tags apparently doesn't have an option to change the man
+	#page number
+	for j in `ls *.man`;do
+		sed -i "1s/ 1 / 8 /" $j
+		mv $j `echo $j | sed "s/\.man//"`.8
+	done
+	)
+done
+
---
+++ trunk/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# install script for salixtools
+
+cd $(dirname $0)
+install -d -m 755 $DESTDIR/usr/sbin
+install -d -m 755 $DESTDIR/usr/share/salixtools/servicesetup
+install -d -m 755 $DESTDIR/etc/rc.d/desc.d
+
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service; do
+	install -m 755 $i/$i $DESTDIR/usr/sbin/
+	for j in `ls $i/locale/*.mo`; do
+		install -d -m 755 \
+		$DESTDIR/usr/share/locale/`basename $j|sed "s/.mo//"`/LC_MESSAGES \
+		2> /dev/null
+		install -m 644 $j \
+		$DESTDIR/usr/share/locale/`basename $j|sed "s/.mo//"`/LC_MESSAGES/$i.mo
+	done
+done
+
+install -d -m 755 $DESTDIR/usr/man/man8
+for i in clocksetup keyboardsetup localesetup usersetup servicesetup service; do
+	(
+	cd $i/man
+	for j in `ls *.8`; do
+		install -m644 $j $DESTDIR/usr/man/man8/
+	done
+	)
+done
+
+install -m 644 keyboardsetup/keymaps $DESTDIR/usr/share/salixtools/
+install -m 755 keyboardsetup/rc.numlock $DESTDIR/etc/rc.d/
+
+install -m 644 service/service-blacklist $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 service/shell-colours $DESTDIR/usr/share/salixtools/servicesetup/
+install -m 644 service/standard.txt $DESTDIR/etc/rc.d/desc.d/
+
