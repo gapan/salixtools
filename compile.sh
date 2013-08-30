a0d7a3ea9586b04bce7ed90251e462256e5500ee
George Vlahavas <vlahavas@gmail.com>
2013-08-30 11:47:45 +0300

    Change man page section from within txt2tags
    Don't use sed to change the man page section from 1 to 8. Use the
    postproc feature of txt2tags to do it.

--- compile.sh
+++ compile.sh
@@ -12,12 +12,6 @@ for i in clocksetup keyboardsetup localesetup usersetup servicesetup service upd
 	cd $i/man
 	echo "Compiling $i manpage"
 	txt2tags *.t2t || exit 1
-	#txt2tags apparently doesn't have an option to change the man
-	#page number
-	for j in `ls *.man`;do
-		sed -i "1s/ 1 / 8 /" $j
-		mv $j `echo $j | sed "s/\.man//"`.8
-	done
 	)
 done
 
